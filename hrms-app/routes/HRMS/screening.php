b1fc2e60fe555ae7c366a40c3ab66fbcfc8847a0
Fariz Allawi Ghozi <farizallawi24@gmail.com>
2025-11-03 03:40:09 +0700

    Configure passport for microservice communication

--- hrms-app/routes/HRMS/screening.php
+++ hrms-app/routes/HRMS/screening.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\HRMS\ScreeningController;
 
 
 Route::prefix('HRMS')->group(function () {
-//    Route::middleware(['auth', 'verified', 'role:hrms-user'])->group(function () {
+    Route::middleware(['auth:api'])->group(function () {
       Route::post('/screening/callback', [ScreeningController::class, 'callback'])->name('screening.callback');
       Route::get('/screening/poll/{task_id}', [ScreeningController::class, 'pollTask']);
-//    });
+    });
 });
